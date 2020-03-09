65f4a69108f8e9f111f7b9b22e2c28260aa3013e
katerina <katpaltoglou@gmail.com>
2020-03-09 12:01:52 +0200

    update branch

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import { Table as index_Tablejs } from "./src/index.js";
-var indexjs = index_Tablejs;
-export { indexjs };
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.indexjs = undefined;
+
+var _index = require("./src/index.js");
+
+var indexjs = _index.Table;
+exports.indexjs = indexjs;
--- src/views/theadTr.js
+++ src/views/theadTr.js
@@ -1,18 +1,37 @@
-import Backbone from "backbone";
-import _ from "underscore";
-import KinView from "backbone-kinview";
-import { theadThjs as theadTh_theadThjsjs } from "./theadTh.js";
-import { thjs as th_thjsjs } from "../models/th.js";
+"use strict";
 
-var theadTrjs = KinView.extend({
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.theadTrjs = undefined;
+
+var _backbone = require("backbone");
+
+var _backbone2 = _interopRequireDefault(_backbone);
+
+var _underscore = require("underscore");
+
+var _underscore2 = _interopRequireDefault(_underscore);
+
+var _backboneKinview = require("backbone-kinview");
+
+var _backboneKinview2 = _interopRequireDefault(_backboneKinview);
+
+var _theadTh = require("./theadTh.js");
+
+var _th = require("../models/th.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var theadTrjs = _backboneKinview2.default.extend({
     tagName: 'tr',
     exclusiveState: true,
-    Th: theadTh_theadThjsjs,
-    initialize: function() {
-        this.children.model = th_thjsjs
-        this.render()
+    Th: _theadTh.theadThjs,
+    initialize: function initialize() {
+        this.children.model = _th.thjs;
+        this.render();
     },
-    addCol: function(opts) {
+    addCol: function addCol(opts) {
         var model = this.add({
             view: new this.Th({
                 text: opts.text || '',
@@ -20,23 +39,20 @@ var theadTrjs = KinView.extend({
             }),
             hasState: opts.click ? true : false,
             data: opts.data || {}
-        })
+        });
 
-        var view = model.get('view')
-	    view.listenTo(model, 'change:state', view.renderState)
+        var view = model.get('view');
+        view.listenTo(model, 'change:state', view.renderState);
 
         if (opts.click) {
-            view.delegate(
-                'click',
-                _.bind(this.clickState, this, model, opts.click)
-            )
+            view.delegate('click', _underscore2.default.bind(this.clickState, this, model, opts.click));
         }
 
-        return view
+        return view;
     },
-    clickState: function(model, done) {
-        done.call(this, model)
+    clickState: function clickState(model, done) {
+        done.call(this, model);
     }
 });
 
-export { theadTrjs };
+exports.theadTrjs = theadTrjs;
